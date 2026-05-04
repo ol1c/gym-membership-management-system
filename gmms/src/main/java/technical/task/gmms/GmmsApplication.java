af4853eb248831050eb3ae359b481f5f148032b5
ol1c <oliccc15@gmail.com>
2026-05-04 17:31:08 +0200

    project init

---
+++ gmms/src/main/java/technical/task/gmms/GmmsApplication.java
@@ -0,0 +1,13 @@
+package technical.task.gmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmmsApplication.class, args);
+	}
+
+}
