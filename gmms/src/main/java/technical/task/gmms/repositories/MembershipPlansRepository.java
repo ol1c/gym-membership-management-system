6be20c8f1a1406501fb5a0c1b20f524d5a1aac45
ol1c <oliccc15@gmail.com>
2026-05-04 19:55:19 +0200

    membership repository

---
+++ gmms/src/main/java/technical/task/gmms/repositories/MembershipPlansRepository.java
@@ -0,0 +1,15 @@
+package technical.task.gmms.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import technical.task.gmms.entities.Gym;
+import technical.task.gmms.entities.MembershipPlan;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MembershipPlansRepository extends JpaRepository<MembershipPlan, UUID> {
+    List<MembershipPlan> findAllByGym(Gym gym);
+    List<MembershipPlan> findAllByGymId(UUID gymId);
+}
