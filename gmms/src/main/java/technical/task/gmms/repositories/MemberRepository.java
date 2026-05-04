672082f78a569c0221b2bd120cc78cd68721784a
ol1c <oliccc15@gmail.com>
2026-05-04 20:45:00 +0200

    rename

--- gmms/src/main/java/technical/task/gmms/repositories/MemberRepository.java
+++ gmms/src/main/java/technical/task/gmms/repositories/MemberRepository.java
@@ -2,14 +2,14 @@ package technical.task.gmms.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import technical.task.gmms.entities.Gym;
+import technical.task.gmms.entities.Member;
 import technical.task.gmms.entities.MembershipPlan;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface MembershipPlansRepository extends JpaRepository<MembershipPlan, UUID> {
-    List<MembershipPlan> findAllByGym(Gym gym);
-    List<MembershipPlan> findAllByGymId(UUID gymId);
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+    List<Member> findAllByMembership(MembershipPlan membershipPlan);
+    List<Member> findAllByMembershipId(UUID membershipId);
 }
