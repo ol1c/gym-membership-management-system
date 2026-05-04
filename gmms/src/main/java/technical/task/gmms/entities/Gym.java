58ff7179863adfc4c23a8db25ee9fd86b4770bf3
ol1c <oliccc15@gmail.com>
2026-05-04 20:05:56 +0200

    gym entitie update

--- gmms/src/main/java/technical/task/gmms/entities/Gym.java
+++ gmms/src/main/java/technical/task/gmms/entities/Gym.java
@@ -30,11 +30,11 @@ public class Gym {
 
     public Gym() {    }
 
-    public Gym(UUID id, String name, String phoneNumber, String country, String zipCode, String city, String address) {
+    public Gym(UUID id, String name, String phoneNumber, Address address) {
         this.id = id;
         this.name = name;
         this.phoneNumber = phoneNumber;
-        this.address = new Address(country, zipCode, city, address);
+        this.address = address;
     }
 
     public UUID getId() {
